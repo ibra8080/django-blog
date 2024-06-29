cc23a414e65f96201d4974d4a36d3ea81d58149d
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-06-29 21:27:45 +0000

    Start with view

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as blog_views
 
 urlpatterns = [
-    path("blog/", blog_views.my_blog, name='blog'),
     path('admin/', admin.site.urls),
 ]
